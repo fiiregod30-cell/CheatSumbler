fd4d478605b76f79ef959491c65b1847aaf738e5
fiiregod30-cell <fiiregod3.0@gmail.com>
2026-04-04 00:40:24 -0300

    Update KraftonOffsets.cs

--- KraftonOffsets.cs
+++ KraftonOffsets.cs
@@ -1,1 +1,42 @@
-
+{
+  "versao": "2603.1.2.13",
+  "offsets": {
+    "uworld": "0x114B7E28",
+    "decrypt": "0x0FAD3A28",
+    "gnames": "0x11732A10",
+    "chunk_size": "0x00003E4C",
+    "actors_array": "0x00000138",
+    "actors_for_gc": "0x00000138",
+    "current_level": "0x00000800",
+    "actor_root_component": "0x00000308",
+    "actor_groggy_health": "0x000014c0",
+    "actor_character_name": "0x00001df0",
+    "actor_last_team_num": "0x00002c08",
+    "actor_mesh": "0x000004a0",
+    "mesh_last_submit_time": "0x0000075C",
+    "mesh_last_render_time": "0x00000760",
+    "mesh_always_create_physics": "0x00000488",
+    "actor_root_pos": "0x00000330",
+    "actor_rotation": "0x00001B44",
+    "dropped_item": "0x00000458",
+    "dropped_item_group": "0x1c0",
+    "dropped_item_group_uitem": "0x00000870",
+    "dropped_item_group_item_table": "0x000000B0",
+    "dropped_item_group_uitemid": "0x00000244"
+  },
+  "decryption": {
+    "index": 0,
+    "method1": {
+      "actor_objid": 28,
+      "xor_key": "0x221A03AC",
+      "ror_shift": 17,
+      "final_xor": "0x12082DD7"
+    },
+    "method2": {
+      "actor_objid": 32,
+      "xor_key": "0x7360F24",
+      "ror_shift": 23,
+      "final_xor": "0xB621EC05"
+    }
+  }
+}
